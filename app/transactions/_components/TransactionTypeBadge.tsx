58a441460cf547a7f7602b8006bc6497fddcb020
Alexandre <alexandre_bernardi95@hotmail.com>
2024-11-07 11:32:08 -0300

    feat: add transactions table

---
+++ app/transactions/_components/TransactionTypeBadge.tsx
@@ -0,0 +1,42 @@
+import { Badge } from "@/app/_components/ui/badge";
+import { Transaction, TransactionType } from "@prisma/client";
+import { CircleIcon } from "lucide-react";
+
+interface BadgeTransactionProps {
+  transaction: Transaction;
+}
+
+const TransactionTypeBadge = ({ transaction }: BadgeTransactionProps) => {
+  switch (transaction.type) {
+    case TransactionType.DEPOSIT:
+      return (
+        <Badge className="bg-muted font-bold text-primary hover:bg-muted">
+          <CircleIcon className="mr-2 fill-primary" size={10} />
+          Depósito
+        </Badge>
+      );
+    case TransactionType.EXPENSE:
+      return (
+        <Badge className="bg-muted text-destructive hover:bg-muted">
+          <CircleIcon className="mr-2 fill-destructive" size={10} />
+          Despesa
+        </Badge>
+      );
+    case TransactionType.INVESTMENT:
+      return (
+        <Badge className="bg-muted text-primary hover:bg-muted">
+          <CircleIcon className="mr-2 fill-primary" size={10} />
+          Investimento
+        </Badge>
+      );
+    default:
+      return (
+        <Badge className="bg-muted text-primary hover:bg-muted">
+          <CircleIcon className="mr-2 fill-primary" size={10} />
+          Outro
+        </Badge>
+      );
+  }
+};
+
+export default TransactionTypeBadge;
